6c3e318bc0b998b5de42b41a4b12605edcad654e
Christoph Schmidt-Hieber <christsc@gmx.de>
2010-08-23 13:57:59 +0100

    Factored out pure Python module.

--- conf_mac_release.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-arch_flags=""
-
-./configure CXX="/usr/bin/g++-4.2" CC="/usr/bin/gcc-4.2" LD="/usr/bin/g++-4.2" CFLAGS="$arch_flags" CXXFLAGS="$arch_flags -I/opt/local/include" LDFLAGS="$arch_flags -headerpad_max_install_names -L/opt/local/lib -L/usr/lib" --with-blitz=/opt/local PYTHON=/opt/local/bin/python
