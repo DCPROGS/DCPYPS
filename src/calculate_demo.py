acd6eec13ccd88df97ef9240dc066c8c1c540d86
Christoph Schmidt-Hieber <christsc@gmx.de>
2011-01-09 15:17:15 +0000

    * Added command line parser to demo * Factored out Tk from readmecfile.py

--- src/calculate_demo.py
+++ src/calculate_demo.py
@@ -4,7 +4,19 @@ This script uses scalcslib module to calculate maxPopen, EC50 and
 nH parameters.
 """
 
+DCMAJOR = 0
+DCMINOR = 1
+
 import matplotlib.pyplot as plt
+import argparse
+import sys
+import os
+try:
+    import Tkinter as Tk
+    import tkFileDialog
+    HASTK = True
+except:
+    HASTK = False
 
 import qmatlib as qml
 import scalcslib as scl
@@ -41,7 +53,81 @@ def demoQ():
 
     return  mec.Mechanism(RateList, StateList, ncyc, fastblk, KBlk)
 
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description='DC_PyPs %d.%d demo program.' %(DCMAJOR, DCMINOR))
+    parser.add_argument('-f', '--file', action='store', nargs=1, dest='file',
+                        help='mechanism file (optional); ' \
+                             'will use demo sample if not provided')
+    parser.add_argument('-d', '--demo', action='store_true', dest='demo',
+                        default=False,
+                        help='mechanism file')
+    parser.add_argument('-v', '--version', action='version', 
+                        version='%(prog)s 2.0', 
+                        help='print version information')
+    
+    return parser
+
+def process_args(args):
+    # demo = True to run DC82 numerical example
+    # demo = False to load a mechanism defined in mec file
+    if args.demo:
+    # Case 1: User chooses demo mode. In that case, everything
+    #         else is discarded.
+        if args.file is not None:
+            sys.stdout.write('Running in demo mode. Ignoring mec file.\n')
+        demomec = demoQ()
+    else:
+    # Case 2: Not in demo mode.
+        if args.file is not None:
+        # Case 2a: User supplies a file on the command line 
+            mecfn = args.file[0]
+        else:
+        # Case 2b: No file provided. Attempt to get file name from dialog.
+            if HASTK:
+                mecfn = file_dialog()
+            else:
+                sys.stderr.write("No file provided, couldn't load file dialog, " \
+                                 "aborting now\n")
+                sys.exit(1)
+
+        # Check whether the file is available:
+        if not os.path.exists(mecfn):
+            sys.stderr.write("Couldn't find file %s. Exiting now.\n" % mecfn)
+            sys.exit(1)
+
+        version, meclist, max_mecnum = readmec.get_mec_list(mecfn)
+        print 'mecfile:', mecfn
+        print 'version:', version
+        mecnum, ratenum = readmec.choose_mec_from_list(meclist, max_mecnum)
+        print '\nRead rate set #', ratenum + 1, 'of mec #', mecnum
+        demomec = readmec.load_mec(mecfn, meclist[ratenum][0])
+
+    return demomec
+
+def file_dialog():
+    """
+    Choose mec file to read.
+
+    Returns
+    -------
+    mecfile : filename
+    """
+
+    root = Tk.Tk()
+    mecfile = tkFileDialog.askopenfilename(
+        initialdir='/home/remis/pDC/data',
+        filetypes=[("DC mec", "*.mec"),("DC mec", "*.MEC"),
+                   ("all files", "*")])
+    root.destroy()
+    
+    return mecfile
+    
 if __name__ == "__main__":
+
+    parser = create_parser()
+    args = parser.parse_args()
+    demomec = process_args(args)
 
     tres = 0.00004  # resolution in seconds
     fastBlk = False
@@ -52,21 +138,6 @@ if __name__ == "__main__":
     cmax = 0.01
     tmin = tres
     tmax = 100
-
-    # demo = True to run DC82 numerical example
-    # demo = False to load a mechanism defined in mec file
-    demo = True
-    if demo:
-        demomec = demoQ()
-    else:
-        mecfile = readmec.choose_mecfile()
-        version, meclist, max_mecnum = readmec.get_mec_list(mecfile)
-        print 'mecfile:', mecfile
-        print 'version:', version
-        mecnum, ratenum = readmec.choose_mec_from_list(meclist, max_mecnum)
-        print '\nRead rate set #', ratenum + 1, 'of mec #', mecnum
-        demomec = readmec.load_mec(mecfile, meclist[ratenum][0])
-
 
     #     POPEN CURVE CALCULATIONS
     print '\nCalculating Popen curve parameters:'
@@ -116,6 +187,3 @@ if __name__ == "__main__":
     plt.subplots_adjust(left=None, bottom=0.1, right=None, top=None,
         wspace=0.4, hspace=0.5)
     plt.show()
-
-
-    
