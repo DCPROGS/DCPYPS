Finished fixing likelihood calculation.

dcpyps/qmatlib.py
@@ -743,7 +743,7 @@ def lf0(u, eigvals, Z00):
 
     f = np.zeros(Z00[0].shape)
     for i in range(len(eigvals)):
-        f += np.sum(Z00[i] *  np.exp(-eigvals[i] * u))
+        f += Z00[i] *  np.exp(-eigvals[i] * u)
     return f
 
 def lf1(u, eigvals, Z10, Z11):
@@ -766,7 +766,7 @@ def lf1(u, eigvals, Z10, Z11):
 
     f = np.zeros(Z10[0].shape)
     for i in range(len(eigvals)):
-        f += np.sum((Z10[i] + Z11[i] * u) *  np.exp(-eigvals[i] * u))
+        f += (Z10[i] + Z11[i] * u) *  np.exp(-eigvals[i] * u)
     return f
 
 def eGAF(t, tres, roots, XAF, eigvals, Z00, Z10, Z11):
@@ -792,7 +792,6 @@ def eGAF(t, tres, roots, XAF, eigvals, Z00, Z10, Z11):
     """
 
     eGAFt = np.zeros(XAF[0].shape)
-
     if t < tres * 3: # exact
         if t < tres * 2:
             eGAFt = lf0((t - tres), eigvals, Z00)
@@ -802,10 +801,8 @@ def eGAF(t, tres, roots, XAF, eigvals, Z00, Z10, Z11):
             eGAFt = ff0 - ff1
     else: # asymptotic
         for i in range(len(roots)):
-            eGAFt += XAF[i] * math.exp(-roots[i] * (t - tres))
-
+            eGAFt += XAF[i] * math.exp(roots[i] * (t - tres))
     return eGAFt
-
 
 def XAF(tres, roots, QAA, QFF, QAF, QFA):
     """
@@ -833,12 +830,16 @@ def XAF(tres, roots, QAA, QFF, QAF, QFA):
     for i in range(kA):
         WAA = W(roots[i], tres, QFF, QAA, QAF, QFA, kF, kA)
         rowA[i] = pinf(WAA)
-    colA = np.transpose(nplin.inv(rowA))
+        TrWAA = np.transpose(WAA)
+        colA[i] = pinf(TrWAA)
+    colA = np.transpose(colA)
 
     for i in range(kA):
-        nom = np.dot(np.dot((colA[i].reshape((kA, 1)) * rowA[i]), QAF), expQFF)
+        nom = np.dot(np.dot(np.dot(colA[:,i].reshape((kA, 1)),
+            rowA[i,:].reshape((1, kA))), QAF), expQFF)
         W1A = dW(roots[i], tres, QAF, QFF, QFA, kA, kF)
-        denom = np.dot(np.dot(rowA[i], W1A), colA[i])
+        denom = np.dot(np.dot(rowA[i,:].reshape((1, kA)), W1A),
+            colA[:,i].reshape((kA, 1)))
         X[i] = nom / denom
     return X
 

dcpyps/scalcslib1.py
@@ -863,7 +863,7 @@ def ini_vectors(mec, tres, tcrit, is_chsvec=False):
             coeff = -math.exp(roots[i] * (tcrit - tres)) / roots[i]
             HFA += coeff * XFA[i]
         phiF = qml.phiHJC(eGFA, eGAF, mec.kF)
-
+        
         startB = np.dot(phiF, HFA) / np.dot(np.dot(phiF, HFA), uA)
         endB = np.dot(HFA, uA)
     else:
@@ -900,6 +900,7 @@ def HJClik(bursts, mec, tres, tcrit, is_chsvec=False):
     is_chsvec : bool
         True if CHS vectors should be used (Eq. 5.7, CHS96).
 
+
     Returns
     -------
     loglik : float
@@ -911,6 +912,8 @@ def HJClik(bursts, mec, tres, tcrit, is_chsvec=False):
     # TODO: Errors.
 
     startB, endB = ini_vectors(mec, tres, tcrit, is_chsvec)
+    #print 'startB=', startB
+    #print 'endB=', endB
     Aeigvals, AZ00, AZ10, AZ11 = qml.Zxx(tres, mec.Q, mec.kA, mec.QFF,
         mec.QAF, mec.QFA)
     Aroots = asymptotic_roots(mec, tres, True)
@@ -922,21 +925,27 @@ def HJClik(bursts, mec, tres, tcrit, is_chsvec=False):
 
     loglik = 0
     for ind in bursts:
+        print '\n', ind, 'burst'
         burst = bursts[ind]
         grouplik = startB
         for i in range(len(burst)):
             t = burst[i] * 0.001
             if i % 2 == 0: # open time
+                print '\nOPEN t=', t
                 eGAFt = np.zeros(Axaf[0].shape)
                 eGAFt = qml.eGAF(t, tres, Aroots, Axaf, Aeigvals, AZ00, AZ10, AZ11)
+                print 'eGAFt=', eGAFt
             else: # shut
+                print'\n SHUT t=', t
                 eGAFt = np.zeros(Fxaf[0].shape)
                 eGAFt = qml.eGAF(t, tres, Froots, Fxaf, Feigvals, FZ00, FZ10, FZ11)
+                print 'eGAFt=', eGAFt
             grouplik = np.dot(grouplik, eGAFt)
+            print 'grouplik intermediate=', grouplik
         grouplik = np.dot(grouplik, endB)
-        loglik += math.log(grouplik[0])
+        print 'grouplik=', grouplik[0]
+        loglik += math.log10(grouplik[0])
+        print 'loglik=', loglik
+
     return loglik
 
-#            if grouplik.max() > 1e50:
-#                grouplik = grouplik * 1e-100
-#                print 'grouplik was scaled down'