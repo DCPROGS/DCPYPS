e8b09d27cd2d179102e342bd315d09027fee10e2
Christoph Schmidt-Hieber <christsc@gmx.de>
2011-05-04 00:12:29 +0100

    Added profiler to fitdemo

--- fitdemo.py
+++ fitdemo.py
@@ -11,40 +11,46 @@ from dcpyps import dataset
 from dcpyps import io
 from dcpyps import samples
 
+# profiler 
+import cProfile
 
-# Load demo mechanism (C&H82 numerical example).
-mec = samples.CH82()
-mec.printout(sys.stdout)
+def main():
+    # Load demo mechanism (C&H82 numerical example).
+    mec = samples.CH82()
+    mec.printout(sys.stdout)
 
-tres = 0.0001
-tcrit = 0.004
-conc = 100e-9
+    tres = 0.0001
+    tcrit = 0.004
+    conc = 100e-9
 
-# Prepare parameter dict for simplex
-opts = {}
-opts['mec'] = mec
-opts['conc'] = conc
-opts['tres'] = tres
-opts['tcrit'] = tcrit
-opts['isCHS'] = True
+    # Prepare parameter dict for simplex
+    opts = {}
+    opts['mec'] = mec
+    opts['conc'] = conc
+    opts['tres'] = tres
+    opts['tcrit'] = tcrit
+    opts['isCHS'] = True
 
-# Here should go initial guesses. Nau using rate constants from example.
-rates = mec.get_rates()
+    # Here should go initial guesses. Nau using rate constants from example.
+    rates = mec.rates
 
-# Load data.
-filename = "/dcpyps/samples/CH82.scn"
-ioffset, nint, calfac, header = io.scn_read_header(filename)
-tint, iampl, iprops = io.scn_read_data(filename, ioffset, nint, calfac)
-rec1 = dataset.TimeSeries(filename, header, tint, iampl, iprops)
+    # Load data.
+    filename = "./dcpyps/samples/CH82.scn"
+    ioffset, nint, calfac, header = io.scn_read_header(filename)
+    tint, iampl, iprops = io.scn_read_data(filename, ioffset, nint, calfac)
+    rec1 = dataset.TimeSeries(filename, header, tint, iampl, iprops)
 
-# Impose resolution, get open/shut times and bursts.
-rec1.impose_resolution(tres)
-rec1.get_open_shut_periods()
-rec1.get_bursts(tcrit)
+    # Impose resolution, get open/shut times and bursts.
+    rec1.impose_resolution(tres)
+    rec1.get_open_shut_periods()
+    rec1.get_bursts(tcrit)
 
-# Maximum likelihood fit.
-newrates, loglik = ufl.simplex(rates, rec1.bursts, scl.HJClik,
-    opts, verbose=0)
-mec.set_rates(newrates)
-print "\n Final rate constants:"
-mec.printout(sys.stdout)
+    # Maximum likelihood fit.
+
+    newrates, loglik = ufl.simplex(rates, rec1.bursts, scl.HJClik,
+        opts, verbose=0)
+    mec.rates = newrates
+    print "\n Final rate constants:"
+    mec.printout(sys.stdout)
+
+cProfile.run('main()')
