be870cac1ccae2ad8d6cb712fcb989c867864f8d
Christoph Schmidt-Hieber <christsc@gmx.de>
2011-10-07 10:15:01 +0100

    Made test executable platform agnostic and added it to distribution.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'DC_PyPs'
-copyright = u'2010, Remigijus Lape, Christoph Schmidt-Hieber'
+copyright = u'2011, Remigijus Lape, Christoph Schmidt-Hieber'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
