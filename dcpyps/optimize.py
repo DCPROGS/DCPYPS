Fixed calculation of CHS vectors.

dcpyps/optimize.py
@@ -6,63 +6,53 @@ from math import*
 import qmatlib  as qml
 import scalcslib as scl
 
-def ini_vectors(mec, eGFA, eGAF, expQFF, XFA,
-        roots, tres, tcrit, is_chsvec=False):
-    """
-    Get initial and final vectors, startB and endB, for HJC likelihood
-    calculation (Eqs. 5.5 or 5.7, CHS96).
+def test_CHS(theta, opts):
 
-    Parameters
-    ----------
-    mec : instance of type Mechanism
-    tres : float
-        Time resolution (dead time).
-    tcrit : float
-        Critical gap length (critical shut time).
-    is_chsvec : bool
-        True if CHS vectors should be used (Eq. 5.7, CHS96).
+    mec = opts['mec']
+    conc = opts['conc']
+    tres = opts['tres']
+    tcrit = opts['tcrit']
+    is_chsvec = opts['isCHS']
 
-    Returns
-    -------
-    startB : ndarray, shape (1, kA)
-        Initial vector for openings or initial CHS vector (Eq. 5.11, CHS96).
-    endB : ndarray, shape (kF, 1)
-        Column of 1's or final CHS vector (Eq. 5.8, CHS96).
-    """
+    mec.set_rateconstants(np.exp(theta))
+    mec.set_eff('c', conc)
 
-#    GAF, GFA = qml.iGs(mec.Q, mec.kA, mec.kF)
-#    expQFF = qml.expQt(mec.QFF, tres)
-#    expQAA = qml.expQt(mec.QAA, tres)
-#    eGAF = qml.eGs(GAF, GFA, mec.kA, mec.kF, expQFF)
-#    eGFA = qml.eGs(GFA, GAF, mec.kF, mec.kA, expQAA)
-    uA = np.ones((mec.kA, 1))
+    GAF, GFA = qml.iGs(mec.Q, mec.kA, mec.kF)
+    expQFF = qml.expQt(mec.QFF, tres)
+    expQAA = qml.expQt(mec.QAA, tres)
+    print 'expQAA=', expQAA
+    eGAF = qml.eGs(GAF, GFA, mec.kA, mec.kF, expQFF)
+    eGFA = qml.eGs(GFA, GAF, mec.kF, mec.kA, expQAA)
+    phiF = qml.phiHJC(eGFA, eGAF, mec.kF)
+    startB = qml.phiHJC(eGAF, eGFA, mec.kA)
+    endB = np.ones((mec.kF, 1))
 
-    print 'Froots=', roots
-    print 'Ftaus=', -1/roots
+    Aeigvals, AZ00, AZ10, AZ11 = qml.Zxx(mec.Q, mec.kA, mec.QFF,
+        mec.QAF, mec.QFA, expQFF, True)
+    Aroots = scl.asymptotic_roots(tres,
+        mec.QAA, mec.QFF, mec.QAF, mec.QFA, mec.kA, mec.kF)
+    Feigvals, FZ00, FZ10, FZ11 = qml.Zxx(mec.Q, mec.kA, mec.QAA,
+        mec.QFA, mec.QAF, expQAA, False)
+    Froots = scl.asymptotic_roots(tres,
+        mec.QFF, mec.QAA, mec.QFA, mec.QAF, mec.kF, mec.kA)
 
-    if is_chsvec:
-#        roots = asymptotic_roots(mec, tres, False)
-        HFA = np.zeros((mec.kF, mec.kA))
-#        XFA = qml.XAF(tres, roots, mec.QFF, mec.QAA, mec.QFA,
-#            mec.QAF, expQFF)
-        for i in range(mec.kF):
-            coeff = -exp(roots[i] * (tcrit - tres)) / roots[i]
-            HFA += coeff * XFA[i]
-
-        print 'HFA=', HFA
-        phiF = qml.phiHJC(eGFA, eGAF, mec.kF)
-        print 'phiF=', phiF
-
-        startB = np.dot(phiF, HFA) / np.dot(np.dot(phiF, HFA), uA)
-        endB = np.dot(HFA, uA)
-    else:
-        startB = qml.phiHJC(eGAF, eGFA, mec.kA)
-        endB = np.ones((mec.kF, 1))
-
+    startB, endB = qml.CHSvec(Froots, tres, tcrit,
+        mec.QAA, mec.QFF, mec.QAF, mec.QFA, mec.kA, mec.kF, phiF)
     print 'startB=', startB
     print 'endB=', endB
 
-    return startB, endB
+    t = 0.0010134001973
+    print '\n\n opening t = ', t
+    eGAFt = qml.eGAF(t, tres, Aeigvals, AZ00, AZ10, AZ11, Aroots,
+        mec.QAA, mec.QFF, mec.QAF, mec.QFA, mec.kA, mec.kF, expQFF)
+    print 'eGAFt=', eGAFt
+
+    t = 0.0001
+    print '\n\n shutting t = ', t
+    eGAFt = qml.eGAF(t, tres, Feigvals, FZ00, FZ10, FZ11, Froots,
+        mec.QFF, mec.QAA, mec.QFA, mec.QAF, mec.kF, mec.kA, expQAA)
+    print 'eGAFt=', eGAFt
+
 
 def HJClik(theta, bursts, opts):
     #HJClik(bursts, mec, tres, tcrit, is_chsvec=False):
@@ -118,22 +108,22 @@ def HJClik(theta, bursts, opts):
     expQAA = qml.expQt(mec.QAA, tres)
     eGAF = qml.eGs(GAF, GFA, mec.kA, mec.kF, expQFF)
     eGFA = qml.eGs(GFA, GAF, mec.kF, mec.kA, expQAA)
-
+    phiF = qml.phiHJC(eGFA, eGAF, mec.kF)
+    startB = qml.phiHJC(eGAF, eGFA, mec.kA)
+    endB = np.ones((mec.kF, 1))
 
     Aeigvals, AZ00, AZ10, AZ11 = qml.Zxx(mec.Q, mec.kA, mec.QFF,
         mec.QAF, mec.QFA, expQFF, True)
     Aroots = scl.asymptotic_roots(tres,
         mec.QAA, mec.QFF, mec.QAF, mec.QFA, mec.kA, mec.kF)
-    Axaf = qml.XAF(tres, Aroots, mec.QAA, mec.QFF, mec.QAF, mec.QFA, expQFF)
     Feigvals, FZ00, FZ10, FZ11 = qml.Zxx(mec.Q, mec.kA, mec.QAA,
         mec.QFA, mec.QAF, expQAA, False)
     Froots = scl.asymptotic_roots(tres,
         mec.QFF, mec.QAA, mec.QFA, mec.QAF, mec.kF, mec.kA)
-    Fxaf = qml.XAF(tres, Froots, mec.QFF, mec.QAA, mec.QFA, mec.QAF, expQAA)
-    startB, endB = ini_vectors(mec, eGFA, eGAF, expQAA, Fxaf, Froots,
-        tres, tcrit, is_chsvec)
-#    print 'startB=', startB
-#    print 'endB=', endB
+
+    if is_chsvec:
+        startB, endB = qml.CHSvec(Froots, tres, tcrit,
+            mec.QAA, mec.QFF, mec.QAF, mec.QFA, mec.kA, mec.kF, phiF)
 
     loglik = 0
     for ind in bursts:
@@ -143,10 +133,14 @@ def HJClik(theta, bursts, opts):
             t = burst[i] * 0.001
             if i % 2 == 0: # open time
                 #eGAFt = np.zeros(Axaf[0].shape)
-                eGAFt = qml.eGAF(t, tres, Aroots, Axaf, Aeigvals, AZ00, AZ10, AZ11)
+                eGAFt = qml.eGAF(t, tres, Aeigvals, AZ00, AZ10, AZ11, Aroots,
+                    mec.QAA, mec.QFF, mec.QAF, mec.QFA, mec.kA, mec.kF, expQFF)
+                #eGAFt = qml.eGAF(t, tres, Aroots, Axaf, Aeigvals, AZ00, AZ10, AZ11)
             else: # shut
                 #eGAFt = np.zeros(Fxaf[0].shape)
-                eGAFt = qml.eGAF(t, tres, Froots, Fxaf, Feigvals, FZ00, FZ10, FZ11)
+                eGAFt = qml.eGAF(t, tres, Feigvals, FZ00, FZ10, FZ11, Froots,
+                    mec.QFF, mec.QAA, mec.QFA, mec.QAF, mec.kF, mec.kA, expQAA)
+                #eGAFt = qml.eGAF(t, tres, Froots, Fxaf, Feigvals, FZ00, FZ10, FZ11)
             grouplik = np.dot(grouplik, eGAFt)
             if grouplik.max() > 1e50:
                 grouplik = grouplik * 1e-100

dcpyps/qmatlib.py
@@ -35,6 +35,7 @@ Phil Trans R Soc Lond A 354, 2555-2590.
 
 import numpy as np
 from numpy import linalg as nplin
+import math
 
 #import dcpypsrc
 
@@ -503,7 +504,67 @@ def dARSdS(tres, QAA, QFF, GAF, GFA, expQFF, kA, kF):
 
     return DARS
 
-def eGAF(t, tres, roots, XAF, eigvals, Z00, Z10, Z11):
+def AR(roots, tres, QAA, QFF, QAF, QFA, kA, kF):
+    """
+    """
+
+    R = np.zeros((kA, kA, kA))
+    row = np.zeros((kA, kA))
+    col1 = np.zeros((kA, kA))
+    for i in range(kA):
+        WA = W(roots[i], tres, QAA, QFF, QAF, QFA, kA, kF)
+        row[i] = pinf(WA)
+        AW = np.transpose(WA)
+        col1[i] = pinf(AW)
+    col = col1.transpose()
+
+    for i in range(kA):
+        nom = np.dot(col[:,i].reshape((kA, 1)), row[i,:].reshape((1, kA)))
+        W1A = dW(roots[i], tres, QAF, QFF, QFA, kA, kF)
+        denom = np.dot(np.dot(row[i,:].reshape((1, kA)), W1A),
+            col[:,i].reshape((kA, 1)))
+        R[i] = nom / denom
+
+    return R
+
+def HAF(roots, tres, tcrit, QFF, QAF, kA, kF, R):
+    """
+    """
+
+    expQFF = expQt(QFF, tres)
+    coeff = -np.exp(roots * (tcrit - tres)) / roots
+    HAF = np.zeros((kA, kF))
+    for i in range(kA):
+        HAF += np.dot(R[i], np.dot(QAF, expQFF)) * coeff[i]
+    return HAF
+
+def CHSvec(roots, tres, tcrit, QAA, QFF, QAF, QFA, kA, kF, phiF):
+    """
+    Calculate initial and final CHS vectors for HJC likelihood function
+    (Eqs. 5.5 or 5.7, CHS96).
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    start : ndarray, shape (1, kA)
+        CHS start vector (Eq. 5.11, CHS96).
+    end : ndarray, shape (kF, 1)
+        CHS end vector (Eq. 5.8, CHS96).
+    """
+
+    R = AR(roots, tres, QFF, QAA, QFA, QAF, kF, kA)
+    H = HAF(roots, tres, tcrit, QAA, QFA, kF, kA, R)
+
+    u = np.ones((kA, 1))
+    start = np.dot(phiF, H) / np.dot(np.dot(phiF, H), u)
+    end = np.dot(H, u)
+
+    return start, end
+
+def eGAF(t, tres, eigvals, Z00, Z10, Z11, roots, QAA, QFF, QAF, QFA, kA, kF, expQFF):
+    #TODO: update documentation
     """
     Calculate transition density eGAF(t) for exact (Eq. 3.2, HJC90) and
     asymptotic (Eq. 3.24, HJC90) distribution.
@@ -527,7 +588,7 @@ def eGAF(t, tres, roots, XAF, eigvals, Z00, Z10, Z11):
     eGAFt : array_like, shape(kA, kA, kF)
     """
 
-    #eGAFt = np.zeros(XAF[0].shape)
+    eGAFt = np.empty((kA, kF))
     if t < tres * 3: # exact
         if t < tres * 2:
             eGAFt = f0((t - tres), eigvals, Z00)
@@ -537,50 +598,15 @@ def eGAF(t, tres, roots, XAF, eigvals, Z00, Z10, Z11):
     else: # asymptotic
 #        for i in range(len(roots)):
 #            eGAFt += XAF[i] * math.exp(roots[i] * (t - tres))
-        eGAFt = np.sum(XAF * np.exp(roots *
-            (t - tres)).reshape(XAF.shape[0],1,1), axis=0)
+        R = AR(roots, (t-tres), QAA, QFF, QAF, QFA, kA, kF)
+        for i in range(len(roots)):
+            eGAFt += np.dot(np.dot(R[i], QAF), expQFF) * math.exp(roots[i] * (t - tres))
+
+#        eGAFt = np.sum(XAF * np.exp(roots *
+#            (t - tres)).reshape(XAF.shape[0],1,1), axis=0)
 
     return eGAFt
 
-def XAF(tres, roots, QAA, QFF, QAF, QFA, expQFF):
-    """
-    Calculate coefficients for asymptotic eGAF(t) (Eq. 3.2 HJC90):
-    AR(tres) * QAF * exp(QFF * tres)
-
-    Parameters
-    ----------
-    tres : float
-        Time resolution (dead time).
-    roots : array_like, shape (1, kA)
-        Roots of the asymptotic open time pdf.
-    QAA, QFF, QAF, QFA : array_like
-        Submatrices of Q.
-
-    Returns
-    -------
-    X : array_like, shape(kA, kA, kF)
-    """
-
-    kA = QAA.shape[0]
-    kF = QFF.shape[0]
-    X = np.zeros((kA, kA, kF))
-    rowA = np.zeros((kA, kA))
-    colA = np.zeros((kA, kA))
-    for i in range(kA):
-        WAA = W(roots[i], tres, QFF, QAA, QFA, QAF, kF, kA)
-        rowA[i] = pinf(WAA)[0]
-        TrWAA = np.transpose(WAA)
-        colA[i] = pinf(TrWAA)[0]
-    colA = np.transpose(colA)
-
-    for i in range(kA):
-        nom = np.dot(np.dot(np.dot(colA[:,i].reshape((kA, 1)),
-            rowA[i,:].reshape((1, kA))), QAF), expQFF)
-        W1A = dW(roots[i], tres, QAF, QFF, QFA, kA, kF)
-        denom = np.dot(np.dot(rowA[i,:].reshape((1, kA)), W1A),
-            colA[:,i].reshape((kA, 1)))
-        X[i] = nom / denom
-    return X
 
 def f0(u, eigvals, Z00):
     """

dcpyps/scalcslib.py
@@ -283,24 +283,29 @@ def asymptotic_areas(tres, roots, QAA, QFF, QAF, QFA, kA, kF, GAF, GFA):
     eGAF = qml.eGs(GAF, GFA, kA, kF, expQFF)
     eGFA = qml.eGs(GFA, GAF, kF, kA, expQAA)
     phiA = qml.phiHJC(eGAF, eGFA, kA)
-
+    R = qml.AR(roots, tres, QAA, QFF, QAF, QFA, kA, kF)
+    uF = np.ones((kF,1))
     areas = np.zeros(kA)
-    rowA = np.zeros((kA,kA))
-    colA = np.zeros((kA,kA))
     for i in range(kA):
-        WA = qml.W(roots[i], tres,
-            QAA, QFF, QAF, QFA, kA, kF)
-        rowA[i] = qml.pinf(WA)
-        AW = np.transpose(WA)
-        colA[i] = qml.pinf(AW)
+        areas[i] = ((-1 / roots[i]) *
+            np.dot(phiA, np.dot(np.dot(R[i], np.dot(QAF, expQFF)), uF)))
 
-    for i in range(kA):
-        uF = np.ones((kF,1))
-        nom = np.dot(np.dot(np.dot(np.dot(np.dot(phiA, colA[i]), rowA[i]),
-            QAF), expQFF), uF)
-        W1A = qml.dW(roots[i], tres, QAF, QFF, QFA, kA, kF)
-        denom = -roots[i] * np.dot(np.dot(rowA[i], W1A), colA[i])
-        areas[i] = nom / denom
+#    rowA = np.zeros((kA,kA))
+#    colA = np.zeros((kA,kA))
+#    for i in range(kA):
+#        WA = qml.W(roots[i], tres,
+#            QAA, QFF, QAF, QFA, kA, kF)
+#        rowA[i] = qml.pinf(WA)
+#        AW = np.transpose(WA)
+#        colA[i] = qml.pinf(AW)
+#
+#    for i in range(kA):
+#        uF = np.ones((kF,1))
+#        nom = np.dot(np.dot(np.dot(np.dot(np.dot(phiA, colA[i]), rowA[i]),
+#            QAF), expQFF), uF)
+#        W1A = qml.dW(roots[i], tres, QAF, QFF, QFA, kA, kF)
+#        denom = -roots[i] * np.dot(np.dot(rowA[i], W1A), colA[i])
+#        areas[i] = nom / denom
 
     return areas
 

fitdemo.py
@@ -4,18 +4,11 @@ Maximum likelihood fit demo.
 """
 
 import sys
-import time
-
 import numpy as np
-
-from dcpyps import scalcslib as scl
-from dcpyps import optimize
-from dcpyps import dataset
-from dcpyps import dcio
-from dcpyps import samples
-
-# profiler 
 import cProfile
+
+from dcpyps import optimize
+from dcpyps import samples
 
 def main():
     # Load demo mechanism (C&H82 numerical example).
@@ -37,35 +30,37 @@ def main():
     # Here should go initial guesses. Now using rate constants from example.
     rates = np.log(mec.unit_rates())
 
-    # Load data.
-    filename = "./dcpyps/samples/CH82.scn"
-    ioffset, nint, calfac, header = dcio.scn_read_header(filename)
-    tint, iampl, iprops = dcio.scn_read_data(filename, ioffset, nint, calfac)
-    rec1 = dataset.TimeSeries(filename, header, tint, iampl, iprops)
+    optimize.test_CHS(rates, opts)
 
-    # Impose resolution, get open/shut times and bursts.
-    rec1.impose_resolution(tres)
-    rec1.get_open_shut_periods()
-    rec1.get_bursts(tcrit)
-    print('\nNumber of bursts = {0:d}'.format(len(rec1.bursts)))
-    blength = rec1.get_burst_length_list()
-    print('Average = {0:.3f} millisec'.format(np.average(blength)))
-    print('Range: {0:.3f}'.format(min(blength)) +
-            ' to {0:.3f} millisec'.format(max(blength)))
-    #rec1.print_bursts()
-
-    # Maximum likelihood fit.
-    print ("\nFitting started: %4d/%02d/%02d %02d:%02d:%02d\n"
-            %time.localtime()[0:6])
-    newrates, loglik = optimize.simplexHJC(rates, rec1.bursts, optimize.HJClik,
-        opts, verbose=0)
-    print ("\nFitting finished: %4d/%02d/%02d %02d:%02d:%02d\n"
-            %time.localtime()[0:6])
-    newrates = np.exp(newrates)
-    mec.set_rateconstants(newrates)
-    print "\n Final rate constants:"
-    mec.printout(sys.stdout)
-    print ('\n Final log-likelihood = {0:.6f}'.format(-loglik))
+#    # Load data.
+#    filename = "./dcpyps/samples/CH82.scn"
+#    ioffset, nint, calfac, header = dcio.scn_read_header(filename)
+#    tint, iampl, iprops = dcio.scn_read_data(filename, ioffset, nint, calfac)
+#    rec1 = dataset.TimeSeries(filename, header, tint, iampl, iprops)
+#
+#    # Impose resolution, get open/shut times and bursts.
+#    rec1.impose_resolution(tres)
+#    rec1.get_open_shut_periods()
+#    rec1.get_bursts(tcrit)
+#    print('\nNumber of bursts = {0:d}'.format(len(rec1.bursts)))
+#    blength = rec1.get_burst_length_list()
+#    print('Average = {0:.3f} millisec'.format(np.average(blength)))
+#    print('Range: {0:.3f}'.format(min(blength)) +
+#            ' to {0:.3f} millisec'.format(max(blength)))
+#    #rec1.print_bursts()
+#
+#    # Maximum likelihood fit.
+#    print ("\nFitting started: %4d/%02d/%02d %02d:%02d:%02d\n"
+#            %time.localtime()[0:6])
+#    newrates, loglik = optimize.simplexHJC(rates, rec1.bursts, optimize.HJClik,
+#        opts, verbose=0)
+#    print ("\nFitting finished: %4d/%02d/%02d %02d:%02d:%02d\n"
+#            %time.localtime()[0:6])
+#    newrates = np.exp(newrates)
+#    mec.set_rateconstants(newrates)
+#    print "\n Final rate constants:"
+#    mec.printout(sys.stdout)
+#    print ('\n Final log-likelihood = {0:.6f}'.format(-loglik))
 
 try:
     cProfile.run('main()')