Fixed bug in exact solution calculation.

dcpyps/optimize.py
@@ -358,8 +358,8 @@ def simplex_limits(theta, perfac, args):
     return np.log(th)
 
 def simplex(func, theta, args=None,
-    stpfac=10, reffac=1.0, extfac=2.0, confac=0.5,
-    shrfac=0.5, resfac=10.0, perfac=0.1,
+    stpfac=5, reffac=1.0, extfac=2.0, confac=0.5,
+    shrfac=0.2, resfac=10.0, perfac=0.1,
     errpar=1e-3, errfunc=1e-3,
     maxiter=10000, maxeval=100000,
     display=False):

dcpyps/qmatlib.py
@@ -628,9 +628,9 @@ def eGAF(t, tres, eigvals, Z00, Z10, Z11, roots, R, QAF, expQFF):
     eGAFt : array_like, shape(kA, kF)
     """
 
-    if t < tres * 2: # exact
+    if t < (tres * 2): # exact
         eGAFt = f0((t - tres), eigvals, Z00)
-    elif t < tres * 3:
+    elif t < (tres * 3):
         eGAFt = (f0((t - tres), eigvals, Z00) -
             f1((t - 2 * tres), eigvals, Z10, Z11))
     else: # asymptotic
@@ -740,7 +740,11 @@ def Zxx(Q, kopen, QFF, QAF, QFA, expQFF, open):
         A1 = A[:, kopen:, :kopen]
     D = np.dot(np.dot(A1, expQFF), QFA)
 
-    C11 = D * C00
+    C11 = np.empty((k, kA, kA))
+    #TODO: try to remove 'for' cycles
+    for i in range(k):
+        C11[i] = np.dot(D[i], C00[i])
+
     C10 = np.empty((k, kA, kA))
     #TODO: try to remove 'for' cycles
     for i in range(k):

dcpyps/test/test.py
@@ -109,11 +109,11 @@ class TestDC_PyPs(unittest.TestCase):
         self.assertAlmostEqual(gamma10[2], -5.49855, 4)
         self.assertAlmostEqual(gamma10[3], 0.671548, 6)
         self.assertAlmostEqual(gamma10[4], -99.9617, 4)
-        self.assertAlmostEqual(gamma11[0], 0.787344, 6)
-        self.assertAlmostEqual(gamma11[1], 42275.7, 1)
-        self.assertAlmostEqual(gamma11[2], 3.39701, 5)
-        self.assertAlmostEqual(gamma11[3], -7.81406, 5)
-        self.assertAlmostEqual(gamma11[4], -1.99149e+06, 0)
+        self.assertAlmostEqual(gamma11[0], 0.885141, 6)
+        self.assertAlmostEqual(gamma11[1], 43634.99, 1)
+        self.assertAlmostEqual(gamma11[2], 718.068, 3)
+        self.assertAlmostEqual(gamma11[3], -39.7437, 3)
+        self.assertAlmostEqual(gamma11[4], -1.9832288e+06, 0)
 
     def test_cjumps(self):
 
@@ -238,9 +238,11 @@ class TestDC_PyPs(unittest.TestCase):
         opts['tres'] = self.tres
         opts['tcrit'] = self.tcrit
         opts['isCHS'] = True
+        opts['data'] = rec1.bursts
         rates = np.log(self.mec.unit_rates())
-        lik, theta = scl.HJClik(rates, rec1.bursts, opts)
-        self.assertAlmostEqual(-lik, 5265.970940, 5)
+        lik, theta = scl.HJClik(rates, opts)
+        print 'lik=', lik
+        self.assertAlmostEqual(-lik, 5265.9536156, 5)
 
     def test_popen(self):
 