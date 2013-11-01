c80ef3fdac2b92afe94acd5e15e937598d0475b2
Remigijus Lape <remis.lp@gmail.com>
2013-11-01 18:51:19 +0000

    Qt GUI: scalcs object created

--- dcpyps/QTmain.py
+++ dcpyps/QTmain.py
@@ -3,8 +3,6 @@
 A simple GUI for DC_PyPs project.
 Depends on pyqt and matplotlib modules.
 """
-import sys
-import numpy as np
 
 try:
     from PySide.QtGui import *
@@ -17,19 +15,16 @@ try:
     from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
     from matplotlib.figure import Figure
     from matplotlib import scale as mscale
-    from matplotlib import cm
-    import matplotlib.pyplot as plt
 except:
     raise ImportError("matplotlib module is missing")
 
-import scalcslib as scl
-import popen
 import samples
 import scplotlib as scpl
 
 from myqtlibs.mechmenu import MechMenu
 from myqtlibs.burstmenu import BurstMenu
 from myqtlibs.jumpmenu import JumpMenu
+from myqtlibs.scalcsmenu import ScalcsMenu
 import myqtlibs.myqtcommon as myqtcommon
 
 class QMatGUI(QMainWindow):
@@ -50,31 +45,7 @@ class QMatGUI(QMainWindow):
         self.present_plot = None
 
         self.menuBar().addMenu(MechMenu(self))
-
-        plotMenu = self.menuBar().addMenu('&Plot')
-        plotPopenAction = myqtcommon.createAction(self, "&Popen curve", self.onPlotPopen)
-        plotOpenTimePDFAction = myqtcommon.createAction(self, 
-            "&Open time pdf", self.onPlotOpenTimePDF)
-        plotShutTimePDFAction = myqtcommon.createAction(self, 
-            "&Shut time pdf", self.onPlotShutTimePDF)
-        plotSubsetTimePDFAction = myqtcommon.createAction(self, 
-            "&Subset time pdf", self.onPlotSubsetTimePDF)
-        plotCorrOpenShutAction = myqtcommon.createAction(self, 
-            "&Correlations", self.onPlotOpShCorr)
-        plotAdjacentOpenShutAction = myqtcommon.createAction(self, 
-            "&Open time adjacent to shut time range pdf", self.onPlotOpAdjShacent)
-        plotMeanOpenNextShutAction = myqtcommon.createAction(self, 
-            "&Mean open time preceding/next to shut time", self.onPlotMeanOpNextShut)
-        plotDependencyAction = myqtcommon.createAction(self, 
-            "&Dependency plot", self.onPlotDependency)
-
-        myqtcommon.addActions(plotMenu, (plotOpenTimePDFAction, plotShutTimePDFAction,
-            plotAdjacentOpenShutAction, plotMeanOpenNextShutAction, 
-            plotCorrOpenShutAction, plotDependencyAction,
-            plotSubsetTimePDFAction.setDisabled(True),
-            plotPopenAction))
-        plotMenu.insertSeparator(plotPopenAction)
-
+        self.menuBar().addMenu(ScalcsMenu(self))
         self.menuBar().addMenu(BurstMenu(self))
         self.menuBar().addMenu(JumpMenu(self))
 
@@ -123,261 +94,6 @@ class QMatGUI(QMainWindow):
         self.mainFrame.setLayout(HBox)
         self.setCentralWidget(self.mainFrame)
         
-    def onPlotPopen(self):
-        """
-        Display Popen curve.
-        """
-
-        self.txtPltBox.clear()
-        self.txtPltBox.append('\t===== Popen PLOT =====')
-        self.txtPltBox.append('Resolution = {0:.5g} mikrosec'.
-            format(self.tres * 1000000))
-        self.txtPltBox.append('Ideal curve- red dashed line.')
-        self.txtPltBox.append('HJC curve- blue solid line.')
-        
-        dialog = myqtcommon.ResDlg(self, self.tres)
-        if dialog.exec_():
-            self.tres = dialog.return_par()
-
-        popen.printout(self.mec, self.tres, output=self.log)
-        c, pe, pi = scpl.Popen(self.mec, self.tres)
-        self.present_plot = np.vstack((c, pe, pi))
-
-        self.axes.clear()
-        self.axes.semilogx(c, pe, 'b-', c , pi, 'r--')
-        self.axes.set_ylim(0, 1)
-        self.axes.xaxis.set_ticks_position('bottom')
-        self.axes.yaxis.set_ticks_position('left')
-        self.canvas.draw()
-
-    def onPlotOpShCorr(self):
-        """
-        Display open, shut and open-shut time correlations.
-        """
-
-        self.txtPltBox.clear()
-        self.txtPltBox.append('\t===== OPEN, SHUT, OPEN/SHUT CORRELATION PLOTS =====')
-        self.txtPltBox.append('Agonist concentration = {0:.5g} mikroM'.
-            format(self.conc * 1000000))
-        self.txtPltBox.append('Shut time correlation - red circles.')
-        self.txtPltBox.append('Open time correlation - green circles')
-        self.txtPltBox.append('Open-shut time correlation - blue circles')
-        
-        dialog = myqtcommon.ConcDlg(self, self.conc)
-        if dialog.exec_():
-            self.conc = dialog.return_par()
-        self.mec.set_eff('c', self.conc)
-        scl.printout_correlations(self.mec, output=self.log)
-        # TODO: need dialog to enter lag value. 
-        lag = 5
-        n, roA, roF, roAF = scpl.corr_open_shut(self.mec, lag)
-        self.present_plot = np.vstack((n, roA, roF, roAF))
-
-        self.axes.clear()
-        self.axes.plot(n, roA,'go', n, roF, 'ro', n, roAF, 'bo')
-        self.axes.axhline(y=0, xmin=0, xmax=1, color='k')
-        self.axes.set_xlim(0, 6)
-        self.axes.xaxis.set_ticks_position('bottom')
-        self.axes.yaxis.set_ticks_position('left')
-        self.canvas.draw()
-        
-    def onPlotOpAdjShacent(self):
-        """
-        Display open time adjacent to shut time range pdf.
-        """
-        self.txtPltBox.clear()
-        self.txtPltBox.append('\t===== OPEN TIME ADJACENT TO SHUT TIME RANGE PDF =====')
-        self.txtPltBox.append('Agonist concentration = {0:.5g} mikroM'.
-            format(self.conc * 1000000))
-        self.txtPltBox.append('Ideal open time pdf- red dashed line.')
-        self.txtPltBox.append('Open times adjacent to shut time range pdf- blue solid line.')
-        
-        dialog = myqtcommon.ConcDlg(self, self.conc)
-        if dialog.exec_():
-            self.conc = dialog.return_par()
-        self.mec.set_eff('c', self.conc)
-        # TODO: need dialog to enter lag value. 
-        dialog = myqtcommon.ShutRangeDlg(self)
-        if dialog.exec_():
-            u1, u2 = dialog.return_par()
-        scl.printout_adjacent(self.mec, u1, u2, output=self.log)
-        
-        t, ipdf, ajpdf = scpl.adjacent_open_time_pdf(self.mec, self.tres, u1, u2)
-        self.present_plot = np.vstack((t, ipdf, ajpdf))
-
-        self.axes.clear()
-        self.axes.semilogx(t, ipdf, 'r--', t, ajpdf, 'b-')
-        self.axes.set_yscale('sqrtscale')
-        self.axes.xaxis.set_ticks_position('bottom')
-        self.axes.yaxis.set_ticks_position('left')
-        self.canvas.draw()
-        
-    def onPlotMeanOpNextShut(self):
-        """
-        Display mean open time preceding / next-to shut time plot.
-        """
-        self.txtPltBox.clear()
-        self.txtPltBox.append('\t===== MEAN OPEN TIME PRECEDING / NEXT TO SHUT TIME =====')
-        self.txtPltBox.append('Agonist concentration = {0:.5g} mikroM'.
-            format(self.conc * 1000000))
-        self.txtPltBox.append('Mean open time preceding specified shut time- red dashed line.')
-        self.txtPltBox.append('Mean open time next to specified shut time- blue dashed line.')
-
-        dialog = myqtcommon.ConcResDlg(self, self.conc, self.tres)
-        if dialog.exec_():
-            self.conc, self.tres = dialog.return_par()
-        self.mec.set_eff('c', self.conc)
-        sht, mp, mn = scpl.mean_open_next_shut(self.mec, self.tres)
-        self.present_plot = np.vstack((sht, mp, mn))
-
-        self.axes.clear()
-        self.axes.semilogx(sht, mp, 'r--', sht, mn, 'b--')
-#        self.axes.set_ylim(bottom=0)
-        self.axes.xaxis.set_ticks_position('bottom')
-        self.axes.yaxis.set_ticks_position('left')
-        self.canvas.draw()
-        
-    def onPlotDependency(self):
-        """
-        Display dependency plot.
-        """
-        
-        self.txtPltBox.clear()
-        self.txtPltBox.append('\t===== DEPENDENCY PLOT =====')
-        self.txtPltBox.append('Agonist concentration = {0:.5g} mikroM'.
-            format(self.conc * 1000000))
-        self.txtPltBox.append('Resolution = {0:.5g} mikrosec'.
-            format(self.tres * 1000000))
-        self.txtPltBox.append('X and Y axis are in ms')
-        
-        dialog = myqtcommon.ConcDlg(self, self.conc)
-        if dialog.exec_():
-            self.conc = dialog.return_par()
-        self.mec.set_eff('c', self.conc)
-        to, ts, d = scpl.dependency_plot(self.mec, self.tres, points=128)
-        
-        fig = plt.figure()
-        fig.suptitle('Dependency plot', fontsize=12)
-        ax = fig.gca(projection='3d')
-        to, ts = np.meshgrid(to, ts)
-        surf = ax.plot_surface(to, ts, d, rstride=1, cstride=1, cmap=cm.coolwarm,
-        linewidth=0, antialiased=False)
-        ax.set_zlim(-1.0, 1.0)
-        
-#        def log_10_product(x, pos):
-#            """The two args are the value and tick position.
-#            Label ticks with the product of the exponentiation"""
-#            return '%1i' % (x)
-#        
-#        ax.set_xscale('log')
-#        ax.set_yscale('log')
-#        formatter = FuncFormatter(log_10_product)
-#        ax.xaxis.set_major_formatter(formatter)
-#        ax.yaxis.set_major_formatter(formatter)
-
-        ax.zaxis.set_major_locator(LinearLocator(10))
-        ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-        fig.colorbar(surf, shrink=0.5, aspect=5)
-        plt.show()
-        
-    def onPlotOpenTimePDF(self):
-        """
-        Display open time probability density function.
-        """
-        self.txtPltBox.clear()
-        self.txtPltBox.append('\t===== OPEN TIME PDF =====')
-        self.txtPltBox.append('Agonist concentration = {0:.5g} mikroM'.
-            format(self.conc * 1000000))
-        self.txtPltBox.append('Resolution = {0:.5g} mikrosec'.
-            format(self.tres * 1000000))
-        self.txtPltBox.append('Ideal pdf- red dashed line.')
-        self.txtPltBox.append('Exact pdf- blue solid line.')
-        self.txtPltBox.append('Asymptotic pdf- green solid line.')
-        
-        dialog = myqtcommon.ConcResDlg(self, self.conc, self.tres)
-        if dialog.exec_():
-            self.conc, self.tres = dialog.return_par()
-        self.mec.set_eff('c', self.conc)
-
-        try:
-            scl.printout_occupancies(self.mec, self.tres, output=self.log)
-            scl.printout_distributions(self.mec, self.tres, output=self.log)
-        except:
-            sys.stderr.write("main: Warning: unable to prepare printout.")
-        
-        t, ipdf, epdf, apdf = scpl.open_time_pdf(self.mec, self.tres)
-        self.present_plot = np.vstack((t, ipdf, epdf, apdf))
-
-        self.axes.clear()
-        self.axes.semilogx(t, ipdf, 'r--', t, epdf, 'b-', t, apdf, 'g-')
-        self.axes.set_yscale('sqrtscale')
-        self.axes.xaxis.set_ticks_position('bottom')
-        self.axes.yaxis.set_ticks_position('left')
-        self.canvas.draw()
-
-    def onPlotSubsetTimePDF(self):
-        """
-        """
-
-        self.txtPltBox.clear()
-        self.txtPltBox.append('\t===== SUBSET TIME PDF =====')
-        self.txtPltBox.append('Agonist concentration = {0:.5g} mikroM'.
-            format(self.conc * 1000000))
-        self.txtPltBox.append('Resolution = {0:.5g} mikrosec'.
-            format(self.tres * 1000000))
-        self.txtPltBox.append('Ideal pdf- red dashed line.')
-        self.txtPltBox.append('Subset life time pdf- blue solid line.')
-        
-        dialog = myqtcommon.ConcDlg(self, self.conc)
-        if dialog.exec_():
-            self.conc = dialog.return_par()
-        self.mec.set_eff('c', self.conc)
-        # TODO: need dialog to enter state1 and state2
-        state1 = 8
-        state2 = 10
-
-        t, ipdf, spdf = scpl.subset_time_pdf(self.mec, self.tres, state1, state2)
-        self.present_plot = np.vstack((t, ipdf, s))
-
-        self.axes.clear()
-        self.axes.semilogx(t, spdf, 'b-', t, ipdf, 'r--')
-        self.axes.set_yscale('sqrtscale')
-        self.axes.xaxis.set_ticks_position('bottom')
-        self.axes.yaxis.set_ticks_position('left')
-        self.canvas.draw()
-
-    def onPlotShutTimePDF(self):
-        """
-        Display shut time probability density function.
-        """
-
-        self.txtPltBox.clear()
-        self.txtPltBox.append('\t===== SHUT TIME PDF =====')
-        self.txtPltBox.append('Agonist concentration = {0:.5g} mikroM'.
-            format(self.conc * 1000000))
-        self.txtPltBox.append('Resolution = {0:.5g} mikrosec'.
-            format(self.tres * 1000000))
-        self.txtPltBox.append('Ideal pdf- red dashed line.')
-        self.txtPltBox.append('Exact pdf- blue solid line.')
-        self.txtPltBox.append('Asymptotic pdf- green solid line.')
-        
-        dialog = myqtcommon.ConcResDlg(self, self.conc, self.tres)
-        if dialog.exec_():
-            self.conc, self.tres = dialog.return_par()
-
-        self.mec.set_eff('c', self.conc)
-        scl.printout_tcrit(self.mec, output=self.log)
-        t, ipdf, epdf, apdf = scpl.shut_time_pdf(self.mec, self.tres)
-        self.present_plot = np.vstack((t, ipdf, epdf, apdf))
-
-        self.axes.clear()
-        self.axes.semilogx(t, ipdf, 'r--', t, epdf, 'b-', t, apdf, 'g-')
-        self.axes.set_yscale('sqrtscale')
-        self.axes.xaxis.set_ticks_position('bottom')
-        self.axes.yaxis.set_ticks_position('left')
-        self.canvas.draw()
-
     def onSavePrintOut(self):
         """
         """
---
+++ dcpyps/myqtlibs/scalcsmenu.py
@@ -0,0 +1,312 @@
+import numpy as np
+import matplotlib.pyplot as plt
+try:
+    from PySide.QtGui import *
+    from PySide.QtCore import *
+except:
+    raise ImportError("pyqt module is missing")
+
+from dcpyps import scplotlib as scpl
+from dcpyps import scalcslib as scl
+from dcpyps import popen
+import myqtcommon
+
+class ScalcsMenu(QMenu):
+    """
+    """
+    def __init__(self, parent):
+        super(ScalcsMenu, self).__init__(parent) 
+        self.parent = parent
+        self.setTitle('&Open/Shut')
+        
+        self.my_colour = ["r", "g", "b", "m", "c", "y"]
+
+        plotOpenTimePDFAction = myqtcommon.createAction(self, 
+            "&Open time pdf", self.onPlotOpenTimePDF)
+        plotShutTimePDFAction = myqtcommon.createAction(self, 
+            "&Shut time pdf", self.onPlotShutTimePDF)
+        plotSubsetTimePDFAction = myqtcommon.createAction(self, 
+            "&Subset time pdf", self.onPlotSubsetTimePDF)
+        plotCorrOpenShutAction = myqtcommon.createAction(self, 
+            "&Correlations", self.onPlotOpShCorr)
+        plotAdjacentOpenShutAction = myqtcommon.createAction(self, 
+            "&Open time adjacent to shut time range pdf", 
+            self.onPlotOpAdjShacent)
+        plotMeanOpenNextShutAction = myqtcommon.createAction(self, 
+            "&Mean open time preceding/next to shut time",
+            self.onPlotMeanOpNextShut)
+        plotDependencyAction = myqtcommon.createAction(self, 
+            "&Dependency plot", self.onPlotDependency)
+        plotPopenAction = myqtcommon.createAction(self, "&Popen curve",
+            self.onPlotPopen)
+
+        self.addActions([plotOpenTimePDFAction, plotShutTimePDFAction,
+            plotAdjacentOpenShutAction, plotMeanOpenNextShutAction, 
+            plotCorrOpenShutAction, 
+#            plotDependencyAction,
+#            plotSubsetTimePDFAction.setDisabled(True),
+            plotPopenAction])
+#        self.insertSeparator(plotPopenAction)
+        
+    def onPlotPopen(self):
+        """
+        Display Popen curve.
+        """
+
+        self.parent.txtPltBox.clear()
+        str = ('\t===== Popen PLOT =====\n' +
+            'Resolution = {0:.5g} mikrosec\n'.format(self.parent.tres * 1000000) +
+            'Ideal curve- red dashed line. \nHJC curve- blue solid line.\n')
+        self.parent.txtPltBox.append(str)
+        
+        dialog = myqtcommon.ResDlg(self, self.parent.tres)
+        if dialog.exec_():
+            self.parent.tres = dialog.return_par()
+
+        popen.printout(self.parent.mec, self.parent.tres, output=self.parent.log)
+        c, pe, pi = scpl.Popen(self.parent.mec, self.parent.tres)
+        self.parent.present_plot = np.vstack((c, pe, pi))
+
+        self.parent.axes.clear()
+        self.parent.axes.semilogx(c, pe, 'b-', c , pi, 'r--')
+        self.parent.axes.set_ylim(0, 1)
+        self.parent.axes.xaxis.set_ticks_position('bottom')
+        self.parent.axes.yaxis.set_ticks_position('left')
+        self.parent.canvas.draw()
+
+    def onPlotOpShCorr(self):
+        """
+        Display open, shut and open-shut time correlations.
+        """
+
+        self.parent.txtPltBox.clear()
+        str = ('\t===== OPEN, SHUT, OPEN/SHUT CORRELATION PLOTS =====\n' +
+            'Agonist concentration = {0:.5g} mikroM\n'.
+            format(self.parent.conc * 1000000) +
+            'Shut time correlation - red circles.\n' +
+            'Open time correlation - green circles\n' +
+            'Open-shut time correlation - blue circles')
+        self.parent.txtPltBox.append(str)
+        
+        dialog = myqtcommon.ConcDlg(self, self.parent.conc)
+        if dialog.exec_():
+            self.parent.conc = dialog.return_par()
+        self.parent.mec.set_eff('c', self.parent.conc)
+        scl.printout_correlations(self.parent.mec, output=self.parent.log)
+        # TODO: need dialog to enter lag value. 
+        lag = 5
+        n, roA, roF, roAF = scpl.corr_open_shut(self.parent.mec, lag)
+        self.parent.present_plot = np.vstack((n, roA, roF, roAF))
+
+        self.parent.axes.clear()
+        self.parent.axes.plot(n, roA,'go', n, roF, 'ro', n, roAF, 'bo')
+        self.parent.axes.axhline(y=0, xmin=0, xmax=1, color='k')
+        self.parent.axes.set_xlim(0, 6)
+        self.parent.axes.xaxis.set_ticks_position('bottom')
+        self.parent.axes.yaxis.set_ticks_position('left')
+        self.parent.canvas.draw()
+        
+    def onPlotOpAdjShacent(self):
+        """
+        Display open time adjacent to shut time range pdf.
+        """
+        self.parent.txtPltBox.clear()
+        str = ('\t===== OPEN TIME ADJACENT TO SHUT TIME RANGE PDF =====\n' +
+            'Agonist concentration = {0:.5g} mikroM\n'.
+            format(self.parent.conc * 1000000) +
+            'Ideal open time pdf- red dashed line.\n' +
+            'Open times adjacent to shut time range pdf- blue solid line.\n')
+        self.parent.txtPltBox.append(str)
+        
+        dialog = myqtcommon.ConcDlg(self, self.parent.conc)
+        if dialog.exec_():
+            self.parent.conc = dialog.return_par()
+        self.parent.mec.set_eff('c', self.parent.conc)
+        # TODO: need dialog to enter lag value. 
+        dialog = myqtcommon.ShutRangeDlg(self)
+        if dialog.exec_():
+            u1, u2 = dialog.return_par()
+        scl.printout_adjacent(self.parent.mec, u1, u2, output=self.parent.log)
+        
+        t, ipdf, ajpdf = scpl.adjacent_open_time_pdf(self.parent.mec, 
+            self.parent.tres, u1, u2)
+        self.parent.present_plot = np.vstack((t, ipdf, ajpdf))
+
+        self.parent.axes.clear()
+        self.parent.axes.semilogx(t, ipdf, 'r--', t, ajpdf, 'b-')
+        self.parent.axes.set_yscale('sqrtscale')
+        self.parent.axes.xaxis.set_ticks_position('bottom')
+        self.parent.axes.yaxis.set_ticks_position('left')
+        self.parent.canvas.draw()
+        
+    def onPlotMeanOpNextShut(self):
+        """
+        Display mean open time preceding / next-to shut time plot.
+        """
+        self.parent.txtPltBox.clear()
+        str = ('\t===== MEAN OPEN TIME PRECEDING / NEXT TO SHUT TIME =====\n' +
+            'Agonist concentration = {0:.5g} mikroM\n'.
+            format(self.parent.conc * 1000000) +
+            'Mean open time preceding specified shut time- red dashed line.\n' +
+            'Mean open time next to specified shut time- blue dashed line.')
+        self.parent.txtPltBox.append(str)
+
+        dialog = myqtcommon.ConcResDlg(self, self.parent.conc, self.parent.tres)
+        if dialog.exec_():
+            self.parent.conc, self.parent.tres = dialog.return_par()
+        self.parent.mec.set_eff('c', self.parent.conc)
+        sht, mp, mn = scpl.mean_open_next_shut(self.parent.mec, self.parent.tres)
+        self.parent.present_plot = np.vstack((sht, mp, mn))
+
+        self.parent.axes.clear()
+        self.parent.axes.semilogx(sht, mp, 'r--', sht, mn, 'b--')
+#        self.axes.set_ylim(bottom=0)
+        self.parent.axes.xaxis.set_ticks_position('bottom')
+        self.parent.axes.yaxis.set_ticks_position('left')
+        self.parent.canvas.draw()
+        
+    def onPlotDependency(self):
+        """
+        Display dependency plot.
+        """
+        
+        self.parent.txtPltBox.clear()
+        str = ('\t===== DEPENDENCY PLOT =====\n' +
+            'Agonist concentration = {0:.5g} mikroM'.
+            format(self.parent.conc * 1000000) +
+            'Resolution = {0:.5g} mikrosec'.format(self.parent.tres * 1000000) +
+            'X and Y axis are in ms')
+        self.parent.txtPltBox.append(str)
+        
+        dialog = myqtcommon.ConcDlg(self, self.parent.conc)
+        if dialog.exec_():
+            self.parent.conc = dialog.return_par()
+        self.parent.mec.set_eff('c', self.parent.conc)
+        to, ts, d = scpl.dependency_plot(self.parent.mec, self.parent.tres, points=128)
+        
+        fig = plt.figure()
+        fig.suptitle('Dependency plot', fontsize=12)
+        ax = fig.gca(projection='3d')
+        to, ts = np.meshgrid(to, ts)
+        surf = ax.plot_surface(to, ts, d, rstride=1, cstride=1, cmap=cm.coolwarm,
+        linewidth=0, antialiased=False)
+        ax.set_zlim(-1.0, 1.0)
+        
+#        def log_10_product(x, pos):
+#            """The two args are the value and tick position.
+#            Label ticks with the product of the exponentiation"""
+#            return '%1i' % (x)
+#        
+#        ax.set_xscale('log')
+#        ax.set_yscale('log')
+#        formatter = FuncFormatter(log_10_product)
+#        ax.xaxis.set_major_formatter(formatter)
+#        ax.yaxis.set_major_formatter(formatter)
+
+        ax.zaxis.set_major_locator(LinearLocator(10))
+        ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+        fig.colorbar(surf, shrink=0.5, aspect=5)
+        plt.show()
+        
+    def onPlotOpenTimePDF(self):
+        """
+        Display open time probability density function.
+        """
+        self.parent.txtPltBox.clear()
+        str = ('\t===== OPEN TIME PDF =====\n' +
+            'Agonist concentration = {0:.5g} mikroM\n'.
+            format(self.parent.conc * 1000000) +
+            'Resolution = {0:.5g} mikrosec\n'.format(self.parent.tres * 1000000) +
+            'Ideal pdf- red dashed line.\n' +
+            'Exact pdf- blue solid line.\n' +
+            'Asymptotic pdf- green solid line.')
+        self.parent.txtPltBox.append(str)
+        
+        dialog = myqtcommon.ConcResDlg(self, self.parent.conc, self.parent.tres)
+        if dialog.exec_():
+            self.parent.conc, self.parent.tres = dialog.return_par()
+        self.parent.mec.set_eff('c', self.parent.conc)
+
+        try:
+            scl.printout_occupancies(self.parent.mec, self.parent.tres, 
+                output=self.parent.log)
+            scl.printout_distributions(self.parent.mec, self.parent.tres,
+                output=self.parent.log)
+        except:
+            sys.stderr.write("main: Warning: unable to prepare printout.")
+        
+        t, ipdf, epdf, apdf = scpl.open_time_pdf(self.parent.mec, self.parent.tres)
+        self.parent.present_plot = np.vstack((t, ipdf, epdf, apdf))
+
+        self.parent.axes.clear()
+        self.parent.axes.semilogx(t, ipdf, 'r--', t, epdf, 'b-', t, apdf, 'g-')
+        self.parent.axes.set_yscale('sqrtscale')
+        self.parent.axes.xaxis.set_ticks_position('bottom')
+        self.parent.axes.yaxis.set_ticks_position('left')
+        self.parent.canvas.draw()
+
+    def onPlotSubsetTimePDF(self):
+        """
+        """
+
+        self.parent.txtPltBox.clear()
+        str = ('\t===== SUBSET TIME PDF =====\n' +
+            'Agonist concentration = {0:.5g} mikroM\n'.
+            format(self.parent.conc * 1000000) +
+            'Resolution = {0:.5g} mikrosec\n'.
+            format(self.parent.tres * 1000000) +
+            'Ideal pdf- red dashed line.\n' +
+            'Subset life time pdf- blue solid line.')
+        self.parent.txtPltBox.append(str)
+        
+        dialog = myqtcommon.ConcDlg(self, self.parent.conc)
+        if dialog.exec_():
+            self.parent.conc = dialog.return_par()
+        self.parent.mec.set_eff('c', self.parent.conc)
+        # TODO: need dialog to enter state1 and state2
+        state1 = 8
+        state2 = 10
+
+        t, ipdf, spdf = scpl.subset_time_pdf(self.parent.mec, self.parent.tres,
+            state1, state2)
+        self.parent.present_plot = np.vstack((t, ipdf, s))
+
+        self.parent.axes.clear()
+        self.parent.axes.semilogx(t, spdf, 'b-', t, ipdf, 'r--')
+        self.parent.axes.set_yscale('sqrtscale')
+        self.parent.axes.xaxis.set_ticks_position('bottom')
+        self.parent.axes.yaxis.set_ticks_position('left')
+        self.parent.canvas.draw()
+
+    def onPlotShutTimePDF(self):
+        """
+        Display shut time probability density function.
+        """
+
+        self.parent.txtPltBox.clear()
+        str = ('\t===== SHUT TIME PDF =====\n' +
+            'Agonist concentration = {0:.5g} mikroM\n'.
+            format(self.parent.conc * 1000000) +
+            'Resolution = {0:.5g} mikrosec\n'.
+            format(self.parent.tres * 1000000) +
+            'Ideal pdf- red dashed line.\n' +
+            'Exact pdf- blue solid line.\n' +
+            'Asymptotic pdf- green solid line.')
+        self.parent.txtPltBox.append(str)
+        
+        dialog = myqtcommon.ConcResDlg(self, self.parent.conc, self.parent.tres)
+        if dialog.exec_():
+            self.parent.conc, self.parent.tres = dialog.return_par()
+
+        self.parent.mec.set_eff('c', self.parent.conc)
+        scl.printout_tcrit(self.parent.mec, output=self.parent.log)
+        t, ipdf, epdf, apdf = scpl.shut_time_pdf(self.parent.mec, self.parent.tres)
+        self.parent.present_plot = np.vstack((t, ipdf, epdf, apdf))
+
+        self.parent.axes.clear()
+        self.parent.axes.semilogx(t, ipdf, 'r--', t, epdf, 'b-', t, apdf, 'g-')
+        self.parent.axes.set_yscale('sqrtscale')
+        self.parent.axes.xaxis.set_ticks_position('bottom')
+        self.parent.axes.yaxis.set_ticks_position('left')
+        self.parent.canvas.draw()
