e38f51b22f294197230ad7da97e56ce0f603c24f
Remigijus Lape <r.lape@ucl.ac.uk>
2015-12-11 13:09:27 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,10 @@
 #!/usr/bin/env python
 """Notes
 -----
-DC_PyPs project are pure Python implementations of Q-Matrix formalisms
-for ion channel research. To learn more about kinetic analysis of ion
-channels see the references below.
-
-References
-----------
-CH82: Colquhoun D, Hawkes AG (1982)
-On the stochastic properties of bursts of single ion channel openings
-and of clusters of bursts. Phil Trans R Soc Lond B 300, 1-59.
-
-HJC92: Hawkes AG, Jalali A, Colquhoun D (1992)
-Asymptotic distributions of apparent open times and shut times in a
-single channel record allowing for the omission of brief events.
-Phil Trans R Soc Lond B 337, 383-404.
-
-CH95a: Colquhoun D, Hawkes AG (1995a)
-The principles of the stochastic interpretation of ion channel mechanisms.
-In: Single-channel recording. 2nd ed. (Eds: Sakmann B, Neher E)
-Plenum Press, New York, pp. 397-482.
-
-CH95b: Colquhoun D, Hawkes AG (1995b)
-A Q-Matrix Cookbook.
-In: Single-channel recording. 2nd ed. (Eds: Sakmann B, Neher E)
-Plenum Press, New York, pp. 589-633.
-
-CHS96: Colquhoun D, Hawkes AG, Srodzinski K (1996)
-Joint distributions of apparent open and shut times of single-ion channels
-and maximum likelihood fitting of mechanisms.
-Phil Trans R Soc Lond A 354, 2555-2590.
+DCPYPS is a collection of modules and scripts common to all the DCPROGS repositories. 
+DCPYPS deals with file input/output, specifying kinetic mechanism, creating data structures. 
+SCALCS and HJCFIT depend on DCPYPS.
 """
-
 # Modified from numpy's setup.py
 
 DOCLINES = __doc__.split("\n")
@@ -52,7 +25,7 @@ Operating System :: Unix
 Operating System :: MacOS
 """
 
-NAME                = 'DC_PyPs'
+NAME                = 'DCPYPS'
 AUTHOR              = "Remigijus Lape, Christoph Schmidt-Hieber et al."
 AUTHOR_EMAIL        = "r.lape@ucl.ac.uk"
 # MAINTAINER          = "DC_PyPs Developers"
@@ -61,8 +34,8 @@ DESCRIPTION         = DOCLINES[0]
 LONG_DESCRIPTION    = "\n".join(DOCLINES[2:])
 PACKAGES            = ["dcpyps", "dcpyps.samples"]
 SCRIPTS             = []
-URL                 = "http://code.google.com/p/dc-pyps/"
-DOWNLOAD_URL        = "http://code.google.com/p/dc-pyps/downloads/list"
+URL                 = "https://github.com/DCPROGS/DCPYPS"
+#DOWNLOAD_URL        = "http://code.google.com/p/dc-pyps/downloads/list"
 LICENSE             = 'GPL2'
 CLASSIFIERS         = filter(None, CLASSIFIERS.split('\n'))
 PLATFORMS           = ["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"]
@@ -78,7 +51,7 @@ if os.path.exists('MANIFEST'): os.remove('MANIFEST')
 
 def write_version_py(filename='dcpyps/version.py'):
     cnt = """
-# THIS FILE IS GENERATED FROM DC_PYPS SETUP.PY
+# THIS FILE IS GENERATED FROM DCPYPS SETUP.PY
 short_version = '%(version)s'
 version = '%(version)s'
 full_version = '%(full_version)s'
