faf0b93ecf4ec2ecee517d5bd391a6ce88a046a7
Remis Lape <r.lape@ucl.ac.uk>
2017-09-05 18:27:50 +0100

    sccalcs\ removed from dcpyps.

--- dcpyps/sccalc/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-DC_PyPs Testing
-===============
-
-DC_PyPs unit tests
-"""
-
-from dcpyps.sccalc import *
---
+++ dcpyps/scsim.py
@@ -0,0 +1,63 @@
+import math
+import random
+import numpy as np
+
+from dcpyps.sccalc import scalcslib as scl
+
+def simulate_intervals(mec, tres, state, opamp=5, nintmax=5000):
+    """
+
+    """
+    # Initialise simulation
+    # Cummulative transition probability
+    picum = np.cumsum(transition_probability(mec.Q), axis=1)
+    #Mean lifetime in each state
+    tmean = -1 / mec.Q.diagonal() # in s
+    # interval and transition counters
+    nint, ntrns = 0, 0 
+    # initial state
+    inst = state
+    # amplitude of initial state
+    a = opamp if inst < mec.kA else 0
+    # length of initial interval
+    t = - tmean[inst] * math.log(random.random())
+    # lists to keep intervals and amplitudes
+    tints, ampls = [t], [a]
+    
+    while nint < nintmax-1:
+
+        newst, t, a = next_state(inst, picum, tmean, mec.kA, opamp)
+        ntrns += 1
+        if t < tres:
+            tints[-1] += t
+            a = ampls[-1]
+        else:
+            if ((a != 0 and ampls[-1] != 0) or (a == 0 and ampls[-1] == 0)):
+                tints[-1] += t
+            else:
+                tints.append(t)
+                ampls.append(a)
+                nint += 1
+        inst = newst
+    
+    return np.array(tints), np.array(ampls), np.zeros((len(tints)), dtype='b'), ntrns
+
+def next_state(present, picum, tmean, kA, opamp):
+    """
+    Get next state, its lifetime and amplitude.
+    """
+    possible = np.nonzero(picum[present] >= random.random())[0]
+    next = np.delete(possible, np.where(possible == present))[0]
+    t = random.expovariate(1 / tmean[next])
+    a = opamp if next < kA else 0
+    return next, t, a
+
+def transition_probability(Q):
+    """
+    """
+    k = Q.shape[0]
+    pi = Q.copy()
+    for i in range(k):
+        pi[i] = pi[i] / -Q[i,i]
+        pi[i,i] = 0
+    return pi
